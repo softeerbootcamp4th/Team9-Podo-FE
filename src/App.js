dc2b27bcccba97e322f374a489399384102af073
admin <admin@adminui-noteubug.local>
2024-07-25 10:44:56 +0900

    test: cicd test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <h1 class="text-3xl font-bold underline">못말리는 현기차</h1>
     </div>
   );
 };
