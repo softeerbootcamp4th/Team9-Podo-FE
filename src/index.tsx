6bea6476f5f4fa3dc3dedb80121f23191eebd3d6
jw0097 <jwoo27@hanmail.net>
2024-07-28 19:51:36 +0900

    chore: migration to typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App";
 import "./styles/tailwind.css";
 
 const rootElement = document.getElementById("root");
