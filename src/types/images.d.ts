423bd6b30416b66cb8ba55ab3c01bd8d42aeb90b
admin <admin@adminui-noteubug.local>
2024-07-29 15:55:00 +0900

    feat: image 타입 정의

---
+++ src/types/images.d.ts
@@ -0,0 +1,20 @@
+// images.d.ts
+declare module "*.png" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.jpg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.jpeg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.gif" {
+  const value: string;
+  export default value;
+}
