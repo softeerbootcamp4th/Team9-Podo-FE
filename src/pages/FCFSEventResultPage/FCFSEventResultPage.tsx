b224d54b237ee0b298d8cc425519c4639321c280
admin <admin@adminui-noteubug.local>
2024-07-30 15:28:49 +0900

    feat: FCFS event result layout

--- src/pages/FCFSEventResultPage/FCFSEventResultPage.tsx
+++ src/pages/FCFSEventResultPage/FCFSEventResultPage.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-const FCFSEventResultPage = () => {
-  return <div>FCFSEventResultPage</div>;
+interface FCFSResultInterface {
+  isWinner: boolean;
+}
+
+const FCFSEventResultPage = ({ isWinner }: FCFSResultInterface) => {
+  const [isResultVisible, setIsResultVisible] = useState(false);
+
+  return (
+    <div>
+      <div>오늘의 퀴즈</div>
+      <img src="" alt="자동차" />
+      <img src="" alt="키" />
+      <p>The 2025 셀토스의 원격 스마트 보조 기능을 통해 당첨을 확인해보세요!</p>
+      <button>버튼</button>
+      {isResultVisible && isWinner ? (
+        <>
+          <div>당첨을 축하합니다!</div>
+          <button>Event 2 참여하러 가기</button>
+          <div>Notions</div>
+        </>
+      ) : (
+        <>
+          <div>아쉽지만 다음 기회에...</div>
+          <button>Event 2 참여하러 가기</button>
+          <div>Notions</div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default FCFSEventResultPage;
