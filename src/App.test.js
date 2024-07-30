839ca3973dbf209fd05e1a034c972ce123870d3f
admin <admin@adminui-noteubug.local>
2024-07-30 12:02:07 +0900

    feat: app test오류 해결

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import React from "react";
-import App from "./App.jsx";
+import App from "./App.tsx";
 test("App", () => {
   render(<App />);
 
