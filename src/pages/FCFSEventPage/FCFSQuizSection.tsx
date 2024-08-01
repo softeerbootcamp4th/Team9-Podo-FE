657007d5279cdc96494008a8332cf9b5df1f1871
jw0097 <jwoo27@hanmail.net>
2024-08-01 14:32:47 +0900

    comment: 선착순 퀴즈 컴포넌트 주석 추가

--- src/pages/FCFSEventPage/FCFSQuizSection.tsx
+++ src/pages/FCFSEventPage/FCFSQuizSection.tsx
@@ -6,11 +6,20 @@ interface FCFSQuizSectionProps {
   quizInfo: QuizInfo;
 }
 
+// tailwind constant
 const IS_SELECTED = {
   true: "",
   false: "",
 };
 
+/**
+ * 선착순 퀴즈 문제 섹션 컴포넌트
+ * @param {QuizInfo} quizInfo - 퀴즈 정보 객체
+ * @param {QuizInfo.question} quizInfo.question - 퀴즈 문제
+ * @param {QuizInfo.question} quizInfo.choices - 퀴즈 선택지
+ * @param {QuizInfo.question} quizInfo.answer - 퀴즈 정답
+ * @returns
+ */
 const FCFSQuizSection = ({ quizInfo }: FCFSQuizSectionProps) => {
   const navigate = useNavigate();
 
@@ -25,14 +34,18 @@ const FCFSQuizSection = ({ quizInfo }: FCFSQuizSectionProps) => {
 
   const { answer, question } = quizInfo;
 
+  // 선택지 클릭 핸들러
   const handleChoiceClick = (index: number) => {
     setSelectedIndex(index);
   };
 
+  // 제출버튼 클릭 핸들러
   const handleSubmitClick = () => {
     if (selectedIndex + 1 === parseInt(answer)) {
       navigate("/event1/result");
       console.log("changed");
+    } else {
+      // 에러 토스트 추후 처리
     }
   };
 
