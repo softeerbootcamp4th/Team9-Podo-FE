3f8f66b898b9ec303c68bd2eeed82a86b671a82f
admin <admin@adminui-noteubug.local>
2024-08-01 10:14:57 +0900

    fix: stories 오류 수정, 라우팅 테스트도 가능하게 memory router 를 template으로 설정

--- src/pages/randomEventPage/RandomQuizSection.stories.tsx
+++ src/pages/randomEventPage/RandomQuizSection.stories.tsx
@@ -1,24 +1,40 @@
 import React from "react";
 import RandomQuizSection from "./RandomQuizSection";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import RandomEventResultPage from "../randomEventResultPage/RandomEventResultPage";
 
 export default {
   component: RandomQuizSection,
   title: "RandomQuizSection",
   tags: ["autodocs"],
   excludeStories: /.*Data$/,
-  args: {},
 };
 
-export const Main = {
-  args: {
-    quizIndex: 0,
-    selectedIndex: null,
-  },
+interface pathInterface {
+  initialPath: string;
+}
+
+const Template = ({ initialPath }: pathInterface) => (
+  <MemoryRouter initialEntries={[initialPath]}>
+    <Routes>
+      <Route path="/event2/:quizIndex" element={<RandomQuizSection />} />
+      <Route path="/event2/result" element={<RandomEventResultPage />} />
+    </Routes>
+  </MemoryRouter>
+);
+
+export const Main = Template.bind({});
+Main.args = {
+  initialPath: "/event2/0",
 };
 
-export const WithSelectedOption = {
-  args: {
-    quizIndex: 0,
-    selectedIndex: 0,
-  },
+export const WithSelectedOption = Template.bind({});
+WithSelectedOption.args = {
+  initialPath: "/event2/0",
+};
+
+// 추가로 다른 인덱스를 테스트하려면
+export const AnotherIndex = Template.bind({});
+AnotherIndex.args = {
+  initialPath: "/event2/1",
 };
