6cc638c7822b7a0b9aa5b5ed4824d749409eebcd
leve68 <hanstom000608@gmail.com>
2024-08-05 16:40:53 +0900

    test: jest 요소 작성

--- src/pages/randomEventResultPage/RandomMainSection/RandomMainSection.test.tsx
+++ src/pages/randomEventResultPage/RandomMainSection/RandomMainSection.test.tsx
@@ -1,1 +1,6 @@
-describe("RandomMainSection", () => {});
+describe("RandomMainSection", () => {
+  test("RandomMainSection은 올바르게 랜더링 되어야 한다.", () => {});
+  test("isAuth가 false일 때 버튼은 본인인증하고 이벤트 참여하기가 보여야 한다.", () => {});
+  test("isAuth가 ture일 때 버튼은 이벤트 참여하기가 보여야 한다.", () => {});
+  test("isRandomEnd가 true일 때 버튼은 비활성화 되어야 한다.", () => {});
+});
