3f8ec1d37231597490ec8e1771f186f57b913aa8
jw0097 <jwoo27@hanmail.net>
2024-08-05 13:54:49 +0900

    feat: webpack 설정

--- webpack.common.js
+++ webpack.common.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "build"),
-    publicPath: "",
+    publicPath: "/",
     clean: true,
   },
   module: {
--- webpack.dev.js
+++ webpack.dev.js
@@ -3,7 +3,7 @@ const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   mode: "development",
-  devtool: "inline-source-map",
+  devtool: "eval",
   devServer: {
     port: 8000,
     historyApiFallback: true,
