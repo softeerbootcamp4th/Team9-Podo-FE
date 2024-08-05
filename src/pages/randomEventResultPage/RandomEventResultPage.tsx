0f95c065401a22d48ef095e5ef780cd35ed5a990
leve68 <hanstom000608@gmail.com>
2024-08-05 15:25:59 +0900

    feat: event2 result 의 mainSection layout

--- src/pages/randomEventResultPage/RandomEventResultPage.tsx
+++ src/pages/randomEventResultPage/RandomEventResultPage.tsx
@@ -1,11 +1,35 @@
 import React from "react";
-import RandomMainSection from "./RandomMainSection";
+import RandomMainSection from "./RandomMainSection/RandomMainSection";
 import RandomExpectations from "./RandomExpectations";
 
 const RandomEventResultPage = () => {
   const driverType = "다이나믹한 모험가";
-  const description = "description";
-  const scenarioList = [{ img: "path", title: "title", content: "content" }];
+  const description = [
+    "복잡한 도심 속 ",
+    "안전하고 즐거운 주행경험",
+    "을 제공하는 ",
+    "The 2025 셀토스",
+  ];
+  const scenarioList = [
+    {
+      img: "path",
+      title: "차로 이탈 시 경고 / 자동 조항 보조",
+      content:
+        "정신 없이 바쁜 일상 속에서도 셀토스는 일정 속도 이상 주행 중 방향지시등 스위치 조작 없이 차로 이탈 시 경고 및 자동 조향 보조 기능으로 사용자의 안전을 지켜줍니다.",
+    },
+    {
+      img: "path",
+      title: "원격 제어 주차 및 출차",
+      content:
+        "원격 제어를 통한 주차 및 출차 기능으로 사용자가 좁은 골목길에서도 걱정없이 주차 할 수 있게 돕습니다. ",
+    },
+    {
+      img: "path",
+      title: "네비게이션 기반 크루즈 컨트롤",
+      content:
+        "네비게이션 기반 크루즈 컨트롤을 통해 고속도로 주행 시, 도로 상황에 맞춰 안전한 속도로 주행하도록 도와줍니다.",
+    },
+  ];
 
   return (
     <div>
@@ -18,10 +42,6 @@ const RandomEventResultPage = () => {
         description={description}
         scenarioList={scenarioList}
       />
-      <div>
-        <p>이벤트 응모하고 시그니엘 숙박권 받자!!</p>
-        <button>이벤트 참여하기</button>
-      </div>
       <RandomExpectations />
     </div>
   );
---
+++ src/pages/randomEventResultPage/RandomMainSection/RandomMainSection.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import Button from "../../../components/common/Button/Button";
+
+interface RandomMainInterface {
+  description: Array<string>;
+  scenarioList: Array<ScenarioInterface>;
+}
+
+interface ScenarioInterface {
+  img: string;
+  title: string;
+  content: string;
+}
+
+const RandomMainSection = ({
+  description,
+  scenarioList,
+}: RandomMainInterface) => {
+  return (
+    <div className="w-[94rem]">
+      <div className="relative flex h-[36.25rem] w-[94rem] flex-col gap-6 rounded-[2.5rem] border-white border-opacity-15 bg-white bg-opacity-10 p-10 backdrop-blur-lg">
+        <div className="absolute right-0 flex gap-8">
+          <div>다시하기</div>
+          <div>공유하기</div>
+        </div>
+        <div className="font-kia-signature-bold text-title-3 text-gray-400">
+          {description}
+        </div>
+        <hr className="h-[1px] bg-gray-400" />
+        <div className="flex gap-6">
+          {scenarioList.map((scenario, index) => (
+            <div className="flex flex-col gap-3">
+              <p className="font-kia-signature-bold text-body-1-bold text-gray-50">{`${index + 1}.`}</p>
+              <img
+                src={scenario.img}
+                alt="시나리오"
+                className="h-[15.25rem] w-[28.75rem] rounded-xl"
+              />
+              <p className="font-kia-signature-bold text-title-4 text-gray-50">
+                {scenario.title}
+              </p>
+              <p className="w-[26rem] font-kia-signature text-body-1-regular text-gray-300">
+                {scenario.content}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="mt-9 flex flex-col gap-4">
+        <p className="font-kia-signature text-gray-200 flex-center">
+          이벤트 응모하고 시그니엘 숙박권 받자!!
+        </p>
+        <Button
+          size="long"
+          onClick={() => {}}
+          defaultText="본인인증하고 이벤트 참여하기"
+          disabledText="이벤트 참여 완료"
+          isEnabled={true}
+        ></Button>
+      </div>
+    </div>
+  );
+};
+
+export default RandomMainSection;
