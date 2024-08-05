b36873e362e2c95d6eebf4bc9b6f8fd7af0ab1c6
leve68 <hanstom000608@gmail.com>
2024-08-05 18:36:36 +0900

    test: role 추가해서 jest에서 가져올 수 있도록 함

--- src/components/common/Toast/Toast.tsx
+++ src/components/common/Toast/Toast.tsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const Toast = ({ content }) => {
   return (
-    <div className="absolute bottom-4 left-1/2 h-fit w-fit -translate-x-1/2 rounded-3xl bg-tertiary px-6 py-3 font-kia-signature-bold text-body-1-bold text-gray-50">
+    <div
+      role="dialog"
+      className="absolute bottom-4 left-1/2 h-fit w-fit -translate-x-1/2 rounded-3xl bg-tertiary px-6 py-3 font-kia-signature-bold text-body-1-bold text-gray-50"
+    >
       {content}
     </div>
   );
