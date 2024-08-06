dc64310183e22d9688c0cc2be58320433663e517
leve68 <hanstom000608@gmail.com>
2024-08-06 13:57:17 +0900

    feat: Roulette component생성

---
+++ src/components/randomEventPage/Roulette/Roulette.stories.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Roulette from "./Roulette";
+
+export default {
+  component: Roulette,
+  title: "Roulette",
+  tags: ["autodocs"],
+  excludeStories: /.*Data$/,
+  args: {},
+};
+
+export const Main = {
+  args: {},
+};
---
+++ src/components/randomEventPage/Roulette/Roulette.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Roulette = () => {
+  return <div>roulette</div>;
+};
+
+export default Roulette;
