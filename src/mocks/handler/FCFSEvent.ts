02342372a278e7ad7022f9d6105db68f4938eeec
jw0097 <jwoo27@hanmail.net>
2024-08-06 17:33:56 +0900

    feat: msw 선착순 결과 핸들러 작성 및 fetch 함수 작성

--- src/mocks/handler/FCFSEvent.ts
+++ src/mocks/handler/FCFSEvent.ts
@@ -1,9 +1,12 @@
 import { http, HttpResponse } from "msw";
-import { quizInfo } from "../data/FCFSEvent";
+import { FCFSFailResult, FCFSSuccessResult, quizInfo } from "../data/FCFSEvent";
 
 const getQuizInfo = http.get("http://localhost:5000/v1/quiz", () => {
-  // ...and respond to them using this JSON response.
   return HttpResponse.json(quizInfo);
 });
 
-export default [getQuizInfo];
+const getFCFSResult = http.post("http://localhost:5000/v1/quiz", () => {
+  return HttpResponse.json(FCFSSuccessResult);
+});
+
+export default [getQuizInfo, getFCFSResult];
