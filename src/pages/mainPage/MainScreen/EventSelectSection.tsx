8348cd0daa60853d4593023fd72f7a2dd9bde84d
leve68 <hanstom000608@gmail.com>
2024-08-08 16:48:15 +0900

    feat: 이벤트 선택창 옵션 전달 Props

--- src/pages/mainPage/MainScreen/EventSelectSection.tsx
+++ src/pages/mainPage/MainScreen/EventSelectSection.tsx
@@ -1,22 +1,28 @@
 import React from "react";
-import EventSelectOptions from "../../../components/mainPage/MainScreen/EventSelectOptions";
+import EventSelectOptions from "../../../components/mainPage/MainScreen/EventSelectOptions/EventSelectOptions";
+import e1Gift from "../../../assets/images/e1Gift.png";
+import e2Gift from "../../../assets/images/e2Gift.png";
+import Glow from "../../../components/common/Glow/Glow";
 
 const EventSelectSection = () => {
   return (
     <>
-      <div>EventSelectTitle</div>;
-      <EventSelectOptions
-        title="event"
-        description="des"
-        img="url"
-        buttonInfo={{ size: "small", onClick: () => {} }}
-      />
-      <EventSelectOptions
-        title="event"
-        description="des"
-        img="url"
-        buttonInfo={{ size: "small", onClick: () => {} }}
-      />
+      <div className="flex-co relative h-screen w-screen flex-center">
+        <div className="h-full w-full flex-center">
+          <EventSelectOptions
+            title="event 1."
+            description={`매일 선착순 100명! \n퀴즈 풀고 스타벅스 커피 받아가자!`}
+            img={e1Gift}
+            buttonInfo={{ size: "small", isEnabled: true, onClick: () => {} }}
+          />
+          <EventSelectOptions
+            title="event 2."
+            description={`내 운전자 유형에 맞는 셀토스 라이프스타일 추천받고,\n 시그니엘 숙박권 당첨의 기회!`}
+            img={e2Gift}
+            buttonInfo={{ size: "small", isEnabled: true, onClick: () => {} }}
+          />
+        </div>
+      </div>
     </>
   );
 };
