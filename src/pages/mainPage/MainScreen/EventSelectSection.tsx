02a78e0b860b4e48e63d934ef478e8b641752843
leve68 <hanstom000608@gmail.com>
2024-08-08 16:48:44 +0900

    feat: 이벤트 선택창 타이틀

--- src/pages/mainPage/MainScreen/EventSelectSection.tsx
+++ src/pages/mainPage/MainScreen/EventSelectSection.tsx
@@ -8,6 +8,23 @@ const EventSelectSection = () => {
   return (
     <>
       <div className="flex-co relative h-screen w-screen flex-center">
+        <div className="-z-10">
+          {/* z-index 정리 필요 */}
+          <Glow />
+        </div>
+        <div
+          className="absolute top-24 flex w-[36.75rem] items-center justify-center rounded-full p-[1px] font-kia-signature-bold text-title-4 text-gray-50"
+          style={{
+            background:
+              "linear-gradient(93.7deg, #505861 0%, #4B7C83 33.5%, #1B3F72 66.5%, #F2F2F2 100%)",
+          }}
+        >
+          <div className="relative h-full w-full overflow-hidden rounded-full bg-gray-900 px-10 py-6 flex-center">
+            <p className="font-kia-signature-bold text-title-2 text-glow-white">
+              The 2025 셀토스 출시 이벤트
+            </p>
+          </div>
+        </div>
         <div className="h-full w-full flex-center">
           <EventSelectOptions
             title="event 1."
