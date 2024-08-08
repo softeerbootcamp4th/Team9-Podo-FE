dd83d35647138be75bcfffa508381875bf1a9164
leve68 <hanstom000608@gmail.com>
2024-08-08 14:03:53 +0900

    feat: quiz 결과를 navigator이용해서 state를 전달함

--- src/pages/randomEventPage/RandomEventPage.tsx
+++ src/pages/randomEventPage/RandomEventPage.tsx
@@ -1,30 +1,35 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Outlet, useNavigate, useParams } from "react-router";
 import ProgressBar from "../../components/randomEventPage/ProgressBar/ProgressBar";
 import { QUIZ_LIST } from "../../constants/RandomEventData";
+import { AnswerInterface } from "../../types/RandomEvent";
 
 const RandomEventPage = () => {
   const navigate = useNavigate();
   const { quizIndex } = useParams();
+  const [answer, setAnswer] = useState<AnswerInterface>({
+    answer1: "",
+    answer2: "",
+    answer3: "",
+    answer4: "",
+  });
 
   const currentIndex = quizIndex ? parseInt(quizIndex, 10) : 0;
   const maxIndex = QUIZ_LIST.length - 1;
   const quizInfo = QUIZ_LIST[currentIndex];
 
-  const handleClick = () => {
-    return new Promise<void>((resolve) => {
-      setTimeout(() => {
-        const nextQuizIndex = currentIndex + 1;
+  const handleClick = (newAnswer: AnswerInterface) => {
+    setAnswer(newAnswer);
+    console.log(newAnswer);
+    setTimeout(() => {
+      const nextQuizIndex = currentIndex + 1;
 
-        if (nextQuizIndex < QUIZ_LIST.length) {
-          navigate(`/event2/${nextQuizIndex}`);
-        } else {
-          navigate("/event2/result");
-        }
-
-        resolve();
-      }, 200);
-    });
+      if (nextQuizIndex < QUIZ_LIST.length) {
+        navigate(`/event2/${nextQuizIndex}`);
+      } else {
+        navigate("/event2/result", { state: newAnswer });
+      }
+    }, 200);
   };
 
   return (
@@ -37,7 +42,14 @@ const RandomEventPage = () => {
       }}
     >
       <ProgressBar currentIndex={currentIndex} maxIndex={maxIndex} />
-      <Outlet context={{ quizInfo, onClick: handleClick }} />
+      <Outlet
+        context={{
+          quizInfo,
+          currentIndex,
+          answer,
+          onClick: handleClick,
+        }}
+      />
     </div>
   );
 };
