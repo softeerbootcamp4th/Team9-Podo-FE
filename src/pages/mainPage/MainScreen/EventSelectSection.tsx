7539aa41d66bd4b39fd541141d054214e787c5ce
leve68 <hanstom000608@gmail.com>
2024-08-09 12:27:25 +0900

    feat: landing 애니메이션 구현

--- src/pages/mainPage/MainScreen/EventSelectSection.tsx
+++ src/pages/mainPage/MainScreen/EventSelectSection.tsx
@@ -1,29 +1,96 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import EventSelectOptions from "../../../components/mainPage/MainScreen/EventSelectOptions/EventSelectOptions";
 import e1Gift from "../../../assets/images/e1Gift.png";
 import e2Gift from "../../../assets/images/e2Gift.png";
 import Glow from "../../../components/common/Glow/Glow";
+import mainCar from "../../../assets/images/mainCar.png";
+import landingCar from "../../../assets/images/landingCar.png";
+import useAnimation from "../../../hooks/useAnimation";
 
 const EventSelectSection = () => {
+  const [isLanding, setIsLanding] = useState(true);
+  const [animation, setAnimation] = useState(false);
+  const { elementRef: landingCarRef, startAnimation: landingCarAnimation } =
+    useAnimation<HTMLImageElement>({
+      startKeyframes: [
+        { transform: "translateX(0)" },
+        { transform: "translateX(-160rem)", display: "none" },
+      ],
+      options: { duration: 500, fill: "forwards", delay: 100 },
+    });
+  const { elementRef: mainCarRef, startAnimation: mainCarAnimation } =
+    useAnimation<HTMLImageElement>({
+      startKeyframes: [{ opacity: "0" }, { opacity: "100" }],
+      options: { duration: 2000, fill: "forwards", delay: 1000 },
+    });
+  const { elementRef: titleRef, startAnimation: titleAnimation } =
+    useAnimation<HTMLDivElement>({
+      startKeyframes: [{ opacity: "0" }, { opacity: "100" }],
+      options: { duration: 2000, fill: "forwards", delay: 600 },
+    });
+  const { elementRef: bgRef, startAnimation: bgAnimation } =
+    useAnimation<HTMLDivElement>({
+      startKeyframes: [{ opacity: "0" }, { opacity: "100" }],
+      options: { duration: 4000, fill: "forwards", delay: 1000 },
+    });
+
+  useEffect(() => {
+    landingCarAnimation();
+    mainCarAnimation();
+    titleAnimation();
+    bgAnimation();
+
+    const landingTimeout = setTimeout(() => {
+      setIsLanding(false);
+    }, 4000);
+
+    return () => clearTimeout(landingTimeout);
+  }, []);
+
   return (
     <div className="relative h-screen w-screen snap-start flex-col flex-center">
-      {/* {<div className="-z-10 overflow-hidden">
-        z-index 정리 필요 
-        <Glow />
-      </div>*/}
-      <div
-        className="absolute top-24 flex w-[36.75rem] items-center justify-center rounded-full p-[1px] font-kia-signature-bold text-title-4 text-gray-50"
-        style={{
-          background:
-            "linear-gradient(93.7deg, #505861 0%, #4B7C83 33.5%, #1B3F72 66.5%, #F2F2F2 100%)",
-        }}
-      >
-        <div className="relative h-full w-full overflow-hidden rounded-full bg-gray-900 px-10 py-6 flex-center">
-          <p className="font-kia-signature-bold text-title-2 text-glow-white">
-            The 2025 셀토스 출시 이벤트
-          </p>
+      {isLanding && (
+        <div className="absolute z-10 h-screen w-screen bg-black">
+          <div className="-z-10 overflow-hidden opacity-0" ref={bgRef}>
+            <Glow />
+          </div>
+          <img
+            src={landingCar}
+            alt=""
+            className="absolute -bottom-96 -right-1/2 h-[64rem] w-[150rem]"
+            ref={landingCarRef}
+          />
         </div>
+      )}
+      <div className="absolute -z-20 h-screen w-screen bg-black"></div>
+      <div className="-z-10 overflow-hidden">
+        <Glow />
       </div>
+      <div
+        className={`${!isLanding ? "top-12" : "top-52"} absolute z-10 flex flex-col items-center justify-start gap-[9rem] transition-all duration-500`}
+      >
+        <div
+          className="w-[36.75rem] rounded-full p-[1px] font-kia-signature-bold text-title-4 text-gray-50 opacity-0"
+          style={{
+            background:
+              "linear-gradient(93.7deg, #505861 0%, #4B7C83 33.5%, #1B3F72 66.5%, #F2F2F2 100%)",
+          }}
+          ref={titleRef}
+        >
+          <div className="relative h-full w-full overflow-hidden rounded-full bg-gray-900 px-10 py-6 flex-center">
+            <p className="font-kia-signature-bold text-title-2 text-glow-white">
+              The 2025 셀토스 출시 이벤트
+            </p>
+          </div>
+        </div>
+        <img
+          src={mainCar}
+          alt=""
+          className={`${isLanding ? "" : "hidden"} opacity-0`} //페이드 아웃 구현필요
+          ref={mainCarRef}
+        />
+      </div>
+
       <div className="h-full w-full flex-center">
         <EventSelectOptions
           title="event 1."
