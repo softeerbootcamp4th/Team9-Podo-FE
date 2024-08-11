968a0e5c3b07250d9f5df79a75d7f3d175acbddb
jw0097 <jwoo27@hanmail.net>
2024-08-11 14:53:28 +0900

    chore: webpack 설정  객체형에서 함수형으로 변경

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,7 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
-module.exports = merge(common, {
+module.exports = merge(common(), {
   mode: "production",
   devtool: "inline-source-map",
 });
