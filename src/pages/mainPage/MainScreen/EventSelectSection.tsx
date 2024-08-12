3094beb5b4429167a7cc577c35d04daf5f80f59d
leve68 <hanstom000608@gmail.com>
2024-08-12 14:00:53 +0900

    fix: 오타 수정

--- src/pages/mainPage/MainScreen/EventSelectSection.tsx
+++ src/pages/mainPage/MainScreen/EventSelectSection.tsx
@@ -11,20 +11,7 @@ import RandomEventSection from "./RandomEventSection";
 
 const EventSelectSection = () => {
   const [isLanding, setIsLanding] = useState(true);
-  const [hoverdIndex, setHoverdIndex] = useState(null);
-  const FCFSRef = useRef<HTMLDivElement | null>(null);
-  const RandomRef = useRef<HTMLDivElement | null>(null);
-
-  const onFCFSClick = () => {
-    FCFSRef.current.scrollIntoView({
-      behavior: "smooth",
-    });
-  };
-  const onRandomClick = () => {
-    RandomRef.current.scrollIntoView({
-      behavior: "smooth",
-    });
-  };
+  const [hoveredIndex, setHoveredIndex] = useState(null);
 
   const { elementRef: landingCarRef, startAnimation: landingCarAnimation } =
     useAnimation<HTMLImageElement>({
@@ -65,75 +52,71 @@ const EventSelectSection = () => {
   }, []);
 
   return (
-    <div className="h-screen w-screen snap-y snap-mandatory overflow-scroll scroll-smooth">
-      <div className="relative h-screen w-screen snap-start flex-col flex-center">
+    <div className="relative h-screen w-screen snap-start flex-col flex-center">
+      <div
+        className={`${!isLanding ? "top-12" : "top-52"} absolute z-10 flex flex-col items-center justify-start gap-[9rem] transition-all duration-500`}
+      >
         <div
-          className={`${!isLanding ? "top-12" : "top-52"} absolute z-10 flex flex-col items-center justify-start gap-[9rem] transition-all duration-500`}
+          className="w-[36.75rem] rounded-full p-[1px] font-kia-signature-bold text-title-4 text-gray-50 opacity-0"
+          style={{
+            background:
+              "linear-gradient(93.7deg, #505861 0%, #4B7C83 33.5%, #1B3F72 66.5%, #F2F2F2 100%)",
+          }}
+          ref={titleRef}
         >
-          <div
-            className="w-[36.75rem] rounded-full p-[1px] font-kia-signature-bold text-title-4 text-gray-50 opacity-0"
-            style={{
-              background:
-                "linear-gradient(93.7deg, #505861 0%, #4B7C83 33.5%, #1B3F72 66.5%, #F2F2F2 100%)",
-            }}
-            ref={titleRef}
-          >
-            <div className="relative h-full w-full overflow-hidden rounded-full bg-gray-900 px-10 py-6 flex-center">
-              <p className="font-kia-signature-bold text-title-2 text-glow-white">
-                The 2025 셀토스 출시 이벤트
-              </p>
-            </div>
+          <div className="relative h-full w-full overflow-hidden rounded-full bg-gray-900 px-10 py-6 flex-center">
+            <p className="font-kia-signature-bold text-title-2 text-glow-white">
+              The 2025 셀토스 출시 이벤트
+            </p>
           </div>
         </div>
-        <img
-          src={mainCar}
-          alt=""
-          className={`absolute top-96 opacity-0`}
-          ref={mainCarRef}
-        />
-
-        {isLanding && (
-          <img
-            src={landingCar}
-            alt=""
-            className="absolute -bottom-96 -right-1/2 h-[64rem] w-[150rem]"
-            ref={landingCarRef}
-          />
-        )}
-
-        {!isLanding && (
-          <div className="h-full w-full animate-fadeIn flex-center">
-            <EventSelectOptions
-              index={0}
-              hoverdIndex={hoverdIndex}
-              setHoverdIndex={setHoverdIndex}
-              title="event 1."
-              description={`매일 선착순 100명! \n퀴즈 풀고 스타벅스 커피 받아가자!`}
-              img={e1Gift}
-              buttonInfo={{
-                size: "small",
-                isEnabled: true,
-                onClick: onFCFSClick,
-              }}
-            />
-            <EventSelectOptions
-              index={1}
-              hoverdIndex={hoverdIndex}
-              setHoverdIndex={setHoverdIndex}
-              title="event 2."
-              description={`내 운전자 유형에 맞는 셀토스 라이프스타일 추천받고,\n 시그니엘 숙박권 당첨의 기회!`}
-              img={e2Gift}
-              buttonInfo={{
-                size: "small",
-                isEnabled: true,
-                onClick: onRandomClick,
-              }}
-            />
-          </div>
-        )}
       </div>
-      <FCFSEventSection ref={FCFSRef}></FCFSEventSection>
-      <RandomEventSection ref={RandomRef}></RandomEventSection>
+      <img
+        src={mainCar}
+        alt=""
+        className={`absolute top-96 opacity-0`}
+        ref={mainCarRef}
+      />
+
+      {isLanding && (
+        <img
+          src={landingCar}
+          alt=""
+          className="absolute -bottom-96 -right-1/2 h-[64rem] w-[150rem]"
+          ref={landingCarRef}
+        />
+      )}
+
+      {!isLanding && (
+        <div className="h-full w-full animate-fadeIn flex-center">
+          <EventSelectOptions
+            index={0}
+            hoveredIndex={hoveredIndex}
+            setHoveredIndex={setHoveredIndex}
+            title="event 1."
+            description={`매일 선착순 100명! \n퀴즈 풀고 스타벅스 커피 받아가자!`}
+            img={e1Gift}
+            buttonInfo={{
+              size: "small",
+              isEnabled: true,
+              onClick: onFCFSClick,
+            }}
+          />
+          <EventSelectOptions
+            index={1}
+            hoveredIndex={hoveredIndex}
+            setHoveredIndex={setHoveredIndex}
+            title="event 2."
+            description={`내 운전자 유형에 맞는 셀토스 라이프스타일 추천받고,\n 시그니엘 숙박권 당첨의 기회!`}
+            img={e2Gift}
+            buttonInfo={{
+              size: "small",
+              isEnabled: true,
+              onClick: onRandomClick,
+            }}
+          />
+        </div>
+      )}
     </div>
   );
 };
