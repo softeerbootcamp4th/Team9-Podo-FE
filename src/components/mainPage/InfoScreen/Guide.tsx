5e73191584ad3309fe778ee3390bdf8faa0f78e6
jw0097 <jwoo27@hanmail.net>
2024-08-14 15:00:12 +0900

    refactor: useInView를 guide 컴포넌트에서 직접 사용

--- src/components/mainPage/InfoScreen/Guide.tsx
+++ src/components/mainPage/InfoScreen/Guide.tsx
@@ -1,13 +1,15 @@
 import React, { ComponentPropsWithRef, ForwardedRef, forwardRef } from "react";
 import InnerHighlight from "../../../assets/svg/InnerHighlight";
+import useInView from "../../../hooks/useInView";
 
-interface GuideProps extends ComponentPropsWithRef<"svg"> {
-  tailwind: string;
-}
+const Guide = () => {
+  const { isInView, elementRef } = useInView<HTMLDivElement>(0.9);
 
-const Guide = ({ tailwind }: GuideProps, ref: ForwardedRef<HTMLDivElement>) => {
   return (
-    <div ref={ref} className={`absolute top-0 h-screen w-screen ${tailwind}`}>
+    <div
+      ref={elementRef}
+      className={`absolute top-0 h-screen w-screen ${isInView ? "opacity-100" : "opacity-0"} transition-opacity duration-500`}
+    >
       <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 font-kia-signature-bold text-title-2 text-white">
         마우스를 움직여 차량 내부를 확인해보세요
       </div>
--- src/pages/mainPage/InfoScreen/InsideSection.tsx
+++ src/pages/mainPage/InfoScreen/InsideSection.tsx
@@ -4,8 +4,6 @@ import Interior from "../../../assets/images/Interior.png";
 import useInView from "../../../hooks/useInView";
 
 const InsideSection = () => {
-  const { isInView, elementRef } = useInView<HTMLDivElement>(0.9);
-
   return (
     <div className="relative flex w-screen snap-start flex-col bg-white">
       <div className="my-12 ml-52 flex flex-col gap-600">
@@ -22,10 +20,7 @@ const InsideSection = () => {
           alt="interior"
           className="absolute top-0 h-screen w-screen snap-start object-cover"
         />
-        <Guide
-          ref={elementRef}
-          tailwind={`${isInView ? "opacity-100" : "opacity-0"} transition-opacity duration-500`}
-        />
+        <Guide />
       </div>
     </div>
   );
