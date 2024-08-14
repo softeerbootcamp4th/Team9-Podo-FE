e501eb3b5528acd00039f53f8a192aa4d41bce22
jw0097 <jwoo27@hanmail.net>
2024-08-14 17:19:19 +0900

    feat: 사용자가 불빛에 마우스를 가져다 대면 설명이 나오는 기능 구현

--- src/assets/svg/InnerHighlight.tsx
+++ src/assets/svg/InnerHighlight.tsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { Dispatch, MouseEvent, SetStateAction } from "react";
 
 interface InnerHighlightProps {
-  tailwind?: string;
+  setDescriptionStates: Dispatch<SetStateAction<Record<InsideInfo, boolean>>>;
+  descriptionStates: Record<InsideInfo, boolean>;
+  setMouseInfo: Dispatch<SetStateAction<Record<"x" | "y", number>>>;
+  mouseInfo: Record<"x" | "y", number>;
 }
 
-const InnerHighlight = ({ tailwind }: InnerHighlightProps) => {
+const InnerHighlight = ({
+  setDescriptionStates,
+  descriptionStates,
+  setMouseInfo,
+  mouseInfo,
+}: InnerHighlightProps) => {
+  const mouseLeaveHandler = (event: MouseEvent<SVGCircleElement>) => {
+    const target = event.currentTarget;
+    setDescriptionStates((prev) => {
+      return { ...prev, [target?.id]: false };
+    });
+  };
+
+  const mouseEnterHandler = (event: MouseEvent<SVGCircleElement>) => {
+    const target = event.currentTarget;
+    if (!descriptionStates[target?.id as InsideInfo]) {
+      setDescriptionStates((prev) => {
+        return { ...prev, [target?.id]: true };
+      });
+      setMouseInfo({ x: event.clientX, y: event.clientY });
+    }
+  };
+
   return (
     <svg
       width="1920"
@@ -13,38 +38,59 @@ const InnerHighlight = ({ tailwind }: InnerHighlightProps) => {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       preserveAspectRatio="xMidYMid slice"
-      className={`${tailwind} block`}
+      className={`block h-screen w-screen`}
     >
-      <rect width="1920" height="1080" fill="black" fill-opacity="0.7" />
+      <rect width="1920" height="1080" fill="black" fillOpacity="0.7" />
       <circle
+        id="light"
         cx="1346"
         cy="586"
         r="46"
         fill="url(#paint0_radial_3376_453)"
-        fill-opacity="0.3"
+        className={`${descriptionStates["light"] ? "opacity-100" : "opacity-50"} transition-opacity`}
+        onMouseEnter={mouseEnterHandler}
+        onMouseLeave={mouseLeaveHandler}
       />
       <circle
-        cx="985"
-        cy="697"
+        id="dial"
+        cx="975"
+        cy="740"
         r="46"
         fill="url(#paint1_radial_3376_453)"
-        fill-opacity="0.3"
+        className={`${descriptionStates["dial"] ? "opacity-100" : "opacity-50"} transition-opacity`}
+        onMouseEnter={mouseEnterHandler}
+        onMouseLeave={mouseLeaveHandler}
       />
       <circle
+        id="display"
         cx="1125"
         cy="345"
         r="46"
         fill="url(#paint2_radial_3376_453)"
-        fill-opacity="0.3"
+        className={`${descriptionStates["display"] ? "opacity-100" : "opacity-50"} transition-opacity`}
+        onMouseEnter={mouseEnterHandler}
+        onMouseLeave={mouseLeaveHandler}
       />
       <circle
+        id="blow"
         cx="1608"
         cy="376"
         r="46"
         fill="url(#paint3_radial_3376_453)"
-        fill-opacity="0.3"
+        className={`${descriptionStates["blow"] ? "opacity-100" : "opacity-50"} transition-opacity`}
+        onMouseEnter={mouseEnterHandler}
+        onMouseLeave={mouseLeaveHandler}
       />
-      <circle cx="558" cy="156" r="46" fill="url(#paint4_radial_3376_453)" />
+      <circle
+        id="headup"
+        cx="558"
+        cy="156"
+        r="46"
+        fill="url(#paint4_radial_3376_453)"
+        className={`${descriptionStates["headup"] ? "opacity-100" : "opacity-50"} transition-opacity`}
+        onMouseEnter={mouseEnterHandler}
+        onMouseLeave={mouseLeaveHandler}
+      />
       <defs>
         <radialGradient
           id="paint0_radial_3376_453"
@@ -54,8 +100,8 @@ const InnerHighlight = ({ tailwind }: InnerHighlightProps) => {
           gradientUnits="userSpaceOnUse"
           gradientTransform="translate(1346 586) rotate(90) scale(46)"
         >
-          <stop stop-color="#A3B5C7" />
-          <stop offset="1" stop-color="#A3B5C7" stop-opacity="0" />
+          <stop stopColor="#A3B5C7" />
+          <stop offset="1" stopColor="#A3B5C7" stopOpacity="0" />
         </radialGradient>
         <radialGradient
           id="paint1_radial_3376_453"
@@ -63,10 +109,10 @@ const InnerHighlight = ({ tailwind }: InnerHighlightProps) => {
           cy="0"
           r="1"
           gradientUnits="userSpaceOnUse"
-          gradientTransform="translate(985 697) rotate(90) scale(46)"
+          gradientTransform="translate(975 740) rotate(90) scale(46)"
         >
-          <stop stop-color="#A3B5C7" />
-          <stop offset="1" stop-color="#A3B5C7" stop-opacity="0" />
+          <stop stopColor="#A3B5C7" />
+          <stop offset="1" stopColor="#A3B5C7" stopOpacity="0" />
         </radialGradient>
         <radialGradient
           id="paint2_radial_3376_453"
@@ -76,8 +122,8 @@ const InnerHighlight = ({ tailwind }: InnerHighlightProps) => {
           gradientUnits="userSpaceOnUse"
           gradientTransform="translate(1125 345) rotate(90) scale(46)"
         >
-          <stop stop-color="#A3B5C7" />
-          <stop offset="1" stop-color="#A3B5C7" stop-opacity="0" />
+          <stop stopColor="#A3B5C7" />
+          <stop offset="1" stopColor="#A3B5C7" stopOpacity="0" />
         </radialGradient>
         <radialGradient
           id="paint3_radial_3376_453"
@@ -87,8 +133,8 @@ const InnerHighlight = ({ tailwind }: InnerHighlightProps) => {
           gradientUnits="userSpaceOnUse"
           gradientTransform="translate(1608 376) rotate(90) scale(46)"
         >
-          <stop stop-color="#A3B5C7" />
-          <stop offset="1" stop-color="#A3B5C7" stop-opacity="0" />
+          <stop stopColor="#A3B5C7" />
+          <stop offset="1" stopColor="#A3B5C7" stopOpacity="0" />
         </radialGradient>
         <radialGradient
           id="paint4_radial_3376_453"
@@ -98,8 +144,8 @@ const InnerHighlight = ({ tailwind }: InnerHighlightProps) => {
           gradientUnits="userSpaceOnUse"
           gradientTransform="translate(558 156) rotate(90) scale(46)"
         >
-          <stop stop-color="#A3B5C7" />
-          <stop offset="1" stop-color="#A3B5C7" stop-opacity="0" />
+          <stop stopColor="#A3B5C7" />
+          <stop offset="1" stopColor="#A3B5C7" stopOpacity="0" />
         </radialGradient>
       </defs>
     </svg>
--- src/components/mainPage/InfoScreen/Guide.tsx
+++ src/components/mainPage/InfoScreen/Guide.tsx
@@ -1,20 +1,26 @@
-import React, {
-  ComponentPropsWithRef,
-  ForwardedRef,
-  forwardRef,
-  useEffect,
-  useState,
-} from "react";
+import React, { forwardRef, useEffect, useState } from "react";
 import InnerHighlight from "../../../assets/svg/InnerHighlight";
 import useInView from "../../../hooks/useInView";
 import {
+  INSIDE_INFO_DATA,
   InsideGuideTextAnimationTime,
   InsideGuideTextShowTime,
 } from "../../../constants/InfoData";
 
+const initialState: Record<InsideInfo, boolean> = {
+  light: false,
+  dial: false,
+  display: false,
+  blow: false,
+  headup: false,
+};
+
 const Guide = () => {
   const [showText, setShowText] = useState(true);
   const [isAnimation, setIsAnimation] = useState(false);
+  const [descriptionStates, setDescriptionStates] = useState(initialState);
+  const [mouseInfo, setMouseInfo] = useState({ x: 0, y: 0 });
+
   const { isInView, elementRef } = useInView<HTMLDivElement>(0.9, () => {
     setShowText(true);
     setIsAnimation(false);
@@ -51,7 +57,33 @@ const Guide = () => {
           마우스를 움직여 차량 내부를 확인해보세요
         </div>
       )}
-      <InnerHighlight />
+      {Object.keys(descriptionStates).map((key) => {
+        if (descriptionStates[key as InsideInfo]) {
+          return (
+            <div
+              key={key}
+              style={{
+                top: `${mouseInfo.y + 80}px`,
+                left: `${mouseInfo.x - 150}px`,
+              }}
+              className={`absolute flex w-[25rem] flex-col gap-2 break-keep ${descriptionStates[key as InsideInfo] ? "opacity-100" : "opacity-0"} transition-all duration-700`}
+            >
+              <div className="font-kia-signature-bold text-title-3 text-gray-50">
+                {INSIDE_INFO_DATA[key as InsideInfo].title}
+              </div>
+              <div className="font-kia-signature-bold text-body-1-regular text-gray-300">
+                {INSIDE_INFO_DATA[key as InsideInfo].description}
+              </div>
+            </div>
+          );
+        }
+      })}
+      <InnerHighlight
+        mouseInfo={mouseInfo}
+        setMouseInfo={setMouseInfo}
+        descriptionStates={descriptionStates}
+        setDescriptionStates={setDescriptionStates}
+      />
     </div>
   );
 };
