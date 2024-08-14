4d64acf2eeb3a8e2bc0d2910f5275b3243ab34c4
leve68 <hanstom000608@gmail.com>
2024-08-14 18:57:30 +0900

    feat: 워드 클라우드 라이브러리 설치

--- src/pages/mainPage/NotificationScreen/NotificationScreen.tsx
+++ src/pages/mainPage/NotificationScreen/NotificationScreen.tsx
@@ -1,13 +1,95 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import * as echarts from "echarts";
+import "echarts-wordcloud";
+import carMask from "../../../assets/images/wordcloud.png";
 
 const NotificationScreen = () => {
   return (
-    <>
+    <div className="h-screen w-screen snap-start">
       <div>title</div>
-      <div>word cloud</div>
+      <WordCloud data={data}></WordCloud>
       <div>Notification</div>
-    </>
+    </div>
   );
 };
 
 export default NotificationScreen;
+
+const data = [
+  { name: "JavaScript", value: 1000 },
+  { name: "TypeScript", value: 800 },
+  { name: "React", value: 600 },
+  { name: "Vue", value: 400 },
+  { name: "JavaScript", value: 1000 },
+  { name: "TypeScript", value: 800 },
+  { name: "React", value: 600 },
+  { name: "Vue", value: 400 },
+  { name: "JavaScript", value: 1000 },
+  { name: "TypeScript", value: 800 },
+  { name: "React", value: 600 },
+  { name: "Vue", value: 400 },
+  { name: "JavaScript", value: 1000 },
+  { name: "TypeScript", value: 800 },
+  { name: "React", value: 600 },
+  { name: "Vue", value: 400 },
+  { name: "JavaScript", value: 1000 },
+  { name: "TypeScript", value: 800 },
+  { name: "React", value: 600 },
+  { name: "Vue", value: 400 },
+  { name: "JavaScript", value: 1000 },
+  { name: "TypeScript", value: 800 },
+  { name: "React", value: 600 },
+  { name: "Vue", value: 400 },
+  { name: "JavaScript", value: 1000 },
+  { name: "TypeScript", value: 800 },
+  { name: "React", value: 600 },
+  { name: "Vue", value: 400 },
+  { name: "JavaScript", value: 1000 },
+  { name: "TypeScript", value: 800 },
+  { name: "React", value: 600 },
+  { name: "Vue", value: 400 },
+];
+
+const WordCloud = ({ data }: any) => {
+  const carMaskURL = require("../../../assets/images/wordcloud.png");
+  const chartRef = useRef(null);
+  useEffect(() => {
+    const chart = echarts.init(chartRef.current);
+
+    const option = {
+      series: [
+        {
+          type: "wordCloud",
+          gridSize: 10,
+          sizeRange: [12, 50],
+          rotationRange: [0, 0],
+          shape: "circle",
+          textStyle: {
+            normal: {
+              color: function () {
+                return (
+                  "rgb(" +
+                  [
+                    Math.round(Math.random() * 255),
+                    Math.round(Math.random() * 255),
+                    Math.round(Math.random() * 255),
+                  ].join(",") +
+                  ")"
+                );
+              },
+            },
+          },
+          data: data,
+        },
+      ],
+    };
+
+    chart.setOption(option);
+
+    return () => {
+      chart.dispose();
+    };
+  }, [data]);
+
+  return <div ref={chartRef} style={{ width: "600px", height: "400px" }}></div>;
+};
