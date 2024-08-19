ed38974193923284cfcd7ee52edea82682f41e61
leve68 <hanstom000608@gmail.com>
2024-08-19 11:30:59 +0900

    refactor: Random quiz section 객체 키값으로 리팩토링

--- src/pages/randomEventPage/RandomQuizSection/RandomQuizSection.tsx
+++ src/pages/randomEventPage/RandomQuizSection/RandomQuizSection.tsx
@@ -6,85 +6,69 @@ import {
   RandomQuizSectionInterface,
 } from "../../../types/RandomEvent";
 
+const ANIMATION_CLASS = {
+  next: "opacity-0 translate-x-[40rem]",
+  current: "opacity-100",
+  prev: "opacity-0 -translate-x-[40rem]",
+};
+
 const RandomQuizSection = () => {
   const { quizInfo, answer, currentIndex, onClick } =
     useOutletContext<RandomQuizSectionInterface>();
   const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
   const [animationClass, setAnimationClass] = useState("");
 
-  const ANIMATION_CLASS = {
-    next: "opacity-0 translate-x-[40rem]",
-    current: "opacity-100",
-    prev: "opacity-0 -translate-x-[40rem]",
+  useEffect(() => {
+    setSelectedIndex(null);
+    startAnimation();
+  }, [quizInfo]);
+
+  const startAnimation = () => {
+    setAnimationClass(ANIMATION_CLASS.next);
+    const timeout = setTimeout(() => {
+      setAnimationClass(ANIMATION_CLASS.current);
+    }, 200);
+    return () => clearTimeout(timeout);
+  };
+
+  const handleAnswerChange = (newAnswer: string): AnswerInterface => {
+    const updatedAnswer = { ...answer };
+
+    const answerKey = `answer${currentIndex + 1}` as keyof AnswerInterface;
+
+    updatedAnswer[answerKey] = newAnswer;
+
+    return updatedAnswer;
   };
 
   const onClickHandler = (index: number) => {
     setAnimationClass(ANIMATION_CLASS.prev);
     setSelectedIndex(index);
 
-    if (index === 0) {
-      onClick(handleAnswerChange("A"));
-    } else if (index === 1) {
-      onClick(handleAnswerChange("B"));
-    }
+    const answerValue = index === 0 ? "A" : "B";
+    onClick(handleAnswerChange(answerValue));
   };
 
-  const handleAnswerChange = (newAnswer: string) => {
-    const updatedAnswer: AnswerInterface = { ...answer };
-
-    switch (currentIndex) {
-      case 0:
-        updatedAnswer.answer1 = newAnswer;
-        break;
-      case 1:
-        updatedAnswer.answer2 = newAnswer;
-        break;
-      case 2:
-        updatedAnswer.answer3 = newAnswer;
-        break;
-      case 3:
-        updatedAnswer.answer4 = newAnswer;
-        break;
-    }
-
-    return updatedAnswer;
-  };
-
-  useEffect(() => {
-    setSelectedIndex(null);
-    setAnimationClass(ANIMATION_CLASS.next);
-    const timeout = setTimeout(() => {
-      setAnimationClass(ANIMATION_CLASS.current);
-    }, 200);
-
-    return () => clearTimeout(timeout);
-  }, [quizInfo]);
+  const renderOptions = () =>
+    quizInfo.optionList.map((option, index) => (
+      <Option
+        key={index}
+        label={option.label}
+        content={option.content}
+        state={selectedIndex === index ? "selected" : "default"}
+        onClick={() => onClickHandler(index)}
+      />
+    ));
 
   return (
     <div
       role="region"
-      className={`flex h-full flex-col items-center justify-evenly transition-all duration-[500ms] ease-in-out ${animationClass} opacity-0`}
+      className={`flex h-full flex-col items-center justify-evenly transition-all duration-[500ms] ease-in-out ${animationClass}`}
     >
       <p className="w-[43rem] break-keep text-center font-kia-signature text-title-2 text-white">
         {quizInfo.question}
       </p>
-      <div className="flex flex-row gap-8">
-        {quizInfo.optionList.map((option, index) => (
-          <Option
-            key={index}
-            label={option.label}
-            content={option.content}
-            state={
-              selectedIndex === index
-                ? "selected"
-                : selectedIndex === null
-                  ? "default"
-                  : "unselected"
-            }
-            onClick={() => onClickHandler(index)}
-          />
-        ))}
-      </div>
+      <div className="flex flex-row gap-8">{renderOptions()}</div>
     </div>
   );
 };
